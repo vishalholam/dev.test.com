af981305c8df12fa071780319af8e5537fe5ecfc
vishalholam <vishalholam@gmail.com>
2014-01-09 15:35:22 +0530

    git status

---
+++ test.php
@@ -0,0 +1,14 @@
+<html>
+<head>this is my title</head>
+<body>
+
+    <div style="color:green" id="EEEE">
+    	this is Body section araa  this is Github testing
+    </div>
+    
+    <div>Current Directory : EEEE<div>
+    
+</body>
+<html>
+</body>
+</html>
