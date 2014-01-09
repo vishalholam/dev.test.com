56ba87ef0592b3d0c0ffbbc06144fc409ce63a59
vishalholam <vishalholam@gmail.com>
2014-01-09 15:47:01 +0530

    ADednewONe

--- test.php
+++ test.php
@@ -1,14 +1,11 @@
 <html>
 <head>this is my title</head>
 <body>
-
     <div style="color:green" id="EEEE">
     	this is Body section araa  this is Github testing
     </div>
-    
+   
     <div>Current Directory : EEEE<div>
-
-
     <div>E is content Directoty</div>    
 
 </body>
